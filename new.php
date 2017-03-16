fca8d54d1d897f99d23cc5b3c02a8393867ecddd
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-16 13:29:38 +0100

    Waiting to see the error

---
+++ new.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+?>
+
+
+
+<html>
+
+<head>
+<title>Trial and error</title>
+
+</head>
+
+<body>
+
+	<p>Git collaboration</p>
+
+	<form action="" method="">
+
+		<p>Username<input type="text" name="username"/></p>
+
+    </form>
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ oyin.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+?>
+
+
+
+<html>
+
+<head>
+<title>Trial and error</title>
+
+</head>
+
+<body>
+
+	<p>Trying out git in different dimensions</p>
+
+	<form action="" method="">
+
+		<p>Username<input type="text" name="username"/></p>
+
+    </form>
+
+
+
+
+
+
+</body>
+
+</html>
