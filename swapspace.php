fcc3328b267d57d4347d783252a325308bf47148
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-16 14:47:00 +0100

    conflicting file

--- swapspace.php
+++ swapspace.php
@@ -5,3 +5,41 @@
 	echo "<p>This is another project</p>";
 
 ?>
+
+
+
+
+html>
+
+
+<head>
+
+<title></title
+
+</head
+
+<body class="home" >
+
+<div class="container">
+
+
+
+<nav class="dropdown">
+
+<ul>
+
+<li>Home</li>
+<li>Contact<li>
+
+</ul>
+
+
+</nav>
+
+</div>
+
+
+</div>
+
+</body>
+</html>
