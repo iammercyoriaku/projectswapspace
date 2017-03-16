61c630d47196501b5231f6d7c2b5a48b97c52185
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-16 14:37:46 +0100

    Conflict

--- project.php
+++ project.php
@@ -3,3 +3,42 @@
 	echo "<p>Our Project</p>";
 
 ?>
+
+
+
+
+
+<html>
+
+
+<head>
+
+<title></title
+
+</head
+
+<body class="home" >
+
+<div class="container">
+
+
+
+<nav class="dropdown">
+
+<ul>
+
+<li>Home</li>
+<li>Contact<li>
+
+</ul>
+
+
+</nav>
+
+</div>
+
+
+</div>
+
+</body>
+</html>
