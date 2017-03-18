c37b01ca32fdabdbaf6ef386aea2d7ed377cc162
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-18 20:41:18 +0100

    login stage one

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+
+		
+
+
+
+
+
+?>
+
+
+
+
+<html>
+
+  <head>
+
+       <title></title>
+
+  </head>
+
+<body>
+
+    <h2>Welcome!!!!!</h2>
+
+    <p>Fill in your login details</p>
+
+
+
+    <?php
+
+
+
+
+
+    ?>
+
+
+
+<form action ="" method="">
+
+		<p>Username<input type= "text" name ="username"/></p>
+
+		<p>Password<input type= "password" name ="password"/></p>
+
+		<input type="submit" name="login" value="Login"/></p> 
+
+</form>
+
+
+
+</body>
+</html>
