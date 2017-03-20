6eb4babafd967f849d47ee368acf4dbe4c9f87bb
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-20 22:40:49 +0100

    ädded corrections

---
+++ home.php
@@ -0,0 +1,103 @@
+<?php
+
+
+
+include("database.php");
+
+
+
+?>
+
+
+<html>
+
+<head>
+
+	<style >
+     body{background-color: grey;
+
+      color: gold;}
+
+     h2{color: gold;
+     	
+        font-size: 35px;
+        
+        font-family: arial;
+        
+        border: 2px solid blue hidden;
+        
+        padding: 2px;}
+
+    p{border: 2px solid hidden;
+     
+     font-size: 25px;}
+
+     div{text-align: center;
+
+     	border: 1px,solid, blue;}
+
+     #diva{text-align: center;
+     	float: right;}
+
+     .home{
+
+        float: left;
+
+
+     }
+
+     a{color: gold;
+
+       font-size: 20px;
+     }
+
+      a:hover{color: green;}
+
+
+	</style>
+<title>Welcome </title>
+</head>
+<body>
+
+
+
+
+
+<h2>Welcome !!!!!!!!!! </h2>
+
+
+
+<div id ="home">
+
+    
+
+    <a href="home.php">Home</a>
+
+
+
+
+</div>
+
+<div>
+
+	
+
+	<a href="login2.php">Click Here Login</a>
+
+</hr>
+
+    <a href="registration.php">Click here to Register</a>
+
+
+
+
+</div>
+
+
+
+
+
+</body>
+
+</html>
+
